e6bc874414828aa85ae6f5feb93f7424aba01881
akashkbarik <akashkbarik12@gmail.com>
2023-02-22 18:00:05 +0530

    added: use case1(UC1)to the program

--- LineComparison.java
+++ LineComparison.java
@@ -3,6 +3,24 @@ package day3.assignment.LineComparison;
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("welcome to LineComparison program ");
+        //points for Line1
+
+        short point_x1 = 10;
+        short point_x2 = 20;
+        short point_y1 = 10;
+        short point_y2 = 30;
+
+        //points for Line2
+
+        short point_a1 = 20;
+        short point_a2 = 30;
+        short point_b1 = 20;
+        short point_b2 = 10;
+
+        float length1 = (float) Math.sqrt(Math.pow(point_x1 - point_x2, 2) + Math.pow(point_y1 - point_y2, 2));
+
+        float length2 = (float) Math.sqrt(Math.pow(point_a1 - point_a2, 2) + Math.pow(point_b1 - point_b2, 2));
+
 
     }
 
